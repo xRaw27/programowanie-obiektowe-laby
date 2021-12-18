91d0a00faa35c596df703decbf3477068d53f541
xRaw27 <d.piwowarski@onet.pl>
2021-12-18 03:48:35 +0100

    small changes in gui

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -1,62 +1,29 @@
 package agh.ics.oop.gui;
 
-import agh.ics.oop.*;
 import javafx.application.Application;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.*;
 
 public class App extends Application {
-//    private Stage primaryStage;
-//    private GridPane grid;
-//    private IWorldMap map;
-//    private IEngine engine;
-//    private final Map<String, Image> loadedImages = new HashMap<>();
 
     public static void main(String[] args) {
         Application.launch(args);
     }
-
-//    @Override
-//    public void init() throws Exception {
-//        super.init();
-//
-////        String[] args = getParameters().getRaw().toArray(new String[0]);
-////        System.out.println(Arrays.toString(args));
-////
-////        try {
-////            List<MoveDirection> directions = OptionsParser.parse(args);
-////            this.map = new GrassField(20);
-////
-////            List<Vector2d> positions = new ArrayList<>(Arrays.asList(new Vector2d(5,5), new Vector2d(7,6)));
-////            this.engine = new SimulationEngineGUI(directions, this.map, positions, this);
-////
-////        } catch(IllegalArgumentException ex) {
-////            System.out.println(ex.getMessage());
-////            System.exit(0);
-////        }
-//
-//    }
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("JavaFX App");
 
         TextField directionsTextField = new TextField();
+        directionsTextField.setPromptText("directions");
         Button startButton = new Button("Start");
+
         VBox vbox = new VBox(20, directionsTextField, startButton);
         vbox.setAlignment(Pos.CENTER);
         VBox.setMargin(directionsTextField, new Insets(0, 20, 0, 20));
@@ -74,6 +41,7 @@ public class App extends Application {
         });
 
         primaryStage.setScene(new Scene(vbox, 400, 200));
+        startButton.requestFocus();
         primaryStage.show();
     }
 
